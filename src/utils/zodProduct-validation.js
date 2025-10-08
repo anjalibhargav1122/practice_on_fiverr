40e3f41d5af1747958e9b7a623c774f0e643a405
sagar <sagarmalyadav0609@gmail.com>
2025-10-08 12:08:30 +0530

    k

--- src/utils/zodProduct-validation.js
+++ src/utils/zodProduct-validation.js
@@ -5,7 +5,6 @@ const productValidation = z.object({
   title: z.string().min(3, "title kam se kam 3 characters ka hona chahiye"),
   description: z.string().min(10, "description kam se kam 10 characters ka hona chahiye"),
   price: z.coerce.number().positive("price positive number hona chahiye"),
-  coverImageUrl: z.string().url("valid URL dena zaroori hai").optional(),
   category: z.string().optional(),
 });
 
