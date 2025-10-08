ce186c9a82c349bcedcee28069142ab4b4d8e3cf
Udiit Parreek <uditp2000@gmail.com>
2025-10-08 14:42:12 +0530

    custom error solved

--- src/utils/error_handler.js
+++ src/utils/error_handler.js
@@ -2,7 +2,8 @@ class CustomError extends Error {
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
-    Error.captureStackTrace(this, this.constructor);
+    this.message = message;
+    // Error.captureStackTrace(this, this.constructor);
   }
 }
 
